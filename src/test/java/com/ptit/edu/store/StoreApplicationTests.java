ab59afefc59e42ada3df7b832d155a4c0fc23fe3
minhhieu <worksfit.dev.team@gmail.com>
2018-10-27 16:19:42 +0700

    first commit

--- src/test/java/com/ptit/edu/store/StoreApplicationTests.java
+++ src/test/java/com/ptit/edu/store/StoreApplicationTests.java
@@ -1,18 +1,22 @@
 package com.ptit.edu.store;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class StoreApplicationTests {
+@RunWith(MockitoJUnitRunner.class)
+public abstract class StoreApplicationTests {
 
-	@Test
+	@Before
 	public void contextLoads() {
+		MockitoAnnotations.initMocks(this);
 	}
 
 }
